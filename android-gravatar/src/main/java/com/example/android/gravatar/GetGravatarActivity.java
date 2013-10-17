ff18232829fa738450100a9b8251b7a8a7e8b8fc
Denis Nelubin <dnelubin@gmail.com>
2013-10-17 22:13:00 +0700

    Added first layout for GetGravatarActivity.

---
+++ android-gravatar/src/main/java/com/example/android/gravatar/GetGravatarActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.gravatar;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class GetGravatarActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.get_gravatar_activity);
+    }
+
+    public void findGravatar(View view) {
+    }
+
+}
