c8a5cd28cede8ff5d4259314e67fe7164d27cae6
Denis Nelubin <dnelubin@gmail.com>
2013-10-21 19:05:39 +0700

    Changed ViewContactsFragment to use LoadManager.

--- android-gravatar/src/main/java/com/example/android/gravatar/ViewContactsFragment.java
+++ android-gravatar/src/main/java/com/example/android/gravatar/ViewContactsFragment.java
@@ -2,36 +2,27 @@ package com.example.android.gravatar;
 
 import android.app.Activity;
 import android.app.ListFragment;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.widget.CursorAdapter;
 import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
 
-public class ViewContactsFragment extends ListFragment {
+public class ViewContactsFragment extends ListFragment
+        implements LoaderManager.LoaderCallbacks<Cursor> {
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        setListAdapter(createAdapter());
-    }
-
-    ListAdapter createAdapter() {
-        Cursor cursor = getActivity().getContentResolver().query(
-                ContactsContract.Data.CONTENT_URI,
-                new String[] {  ContactsContract.Data._ID,
-                                ContactsContract.Contacts.DISPLAY_NAME,
-                                ContactsContract.CommonDataKinds.Email.ADDRESS },
-                ContactsContract.CommonDataKinds.Email.ADDRESS + " is not null" +
-                    " AND " + ContactsContract.Data.MIMETYPE + " = '" +
-                        ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE + "'",
-                null,
-                ContactsContract.Contacts.DISPLAY_NAME);
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
         ListAdapter adapter = new SimpleCursorAdapter(
                 getActivity(),
                 R.layout.person_list_item,
-                cursor,
+                null,
                 new String[] {
                         ContactsContract.Contacts.DISPLAY_NAME,
                         ContactsContract.CommonDataKinds.Email.ADDRESS },
@@ -39,8 +30,37 @@ public class ViewContactsFragment extends ListFragment {
                         R.id.name,
                         R.id.email },
                 0);
+        setListAdapter(adapter);
 
-        return adapter;
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(
+                getActivity(),
+                ContactsContract.Data.CONTENT_URI,
+                new String[] {  ContactsContract.Data._ID,
+                        ContactsContract.Contacts.DISPLAY_NAME,
+                        ContactsContract.CommonDataKinds.Email.ADDRESS },
+                ContactsContract.CommonDataKinds.Email.ADDRESS + " is not null" +
+                        " AND " + ContactsContract.Data.MIMETYPE + " = '" +
+                        ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE + "'",
+                null,
+                ContactsContract.Contacts.DISPLAY_NAME);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> objectLoader, Cursor cursor) {
+        CursorAdapter adapter = (CursorAdapter) getListAdapter();
+        adapter.swapCursor(cursor);
+    }
+
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> objectLoader) {
+        CursorAdapter adapter = (CursorAdapter) getListAdapter();
+        adapter.swapCursor(null);
     }
 
 }
